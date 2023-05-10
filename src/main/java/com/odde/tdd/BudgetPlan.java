4ab7bd7ad9ff8f069b15eea0159afffc96ca6e5d
sarahwu1 <sarahwu1@refinitiv.com>
2023-05-10 23:08:18 +0800

    sarah budget code

--- src/main/java/com/odde/tdd/BudgetPlan.java
+++ src/main/java/com/odde/tdd/BudgetPlan.java
@@ -1,17 +1,54 @@
 package com.odde.tdd;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.temporal.ChronoField;
+import java.util.Collection;
 import java.util.List;
 
 public class BudgetPlan {
+
     private final BudgetRepo repo;
 
     public BudgetPlan(BudgetRepo repo) {
         this.repo = repo;
     }
 
-    public double query(LocalDate start, LocalDate end) {
+    public double query(LocalDate start, LocalDate end){
         List<Budget> budgets = repo.findAll();
-        return 0;
+        double finalBudget = 0.0;
+
+        if (budgets.isEmpty()){
+            return finalBudget;
+        }
+
+        YearMonth startYearMonth = YearMonth.from(start);
+        YearMonth endYearMonth = YearMonth.from(end);
+
+        int startMonthNonBudget = start.getDayOfMonth() - 1;
+        int endMonthNonBudget = end.lengthOfMonth() - end.getDayOfMonth();
+
+        int startMonthIndex = 0;
+        int endMonthIndex = budgets.size();
+
+        for (int i = 0; i < budgets.size(); i++) {
+            if (budgets.get(i).getMonth().equals(startYearMonth)){
+                startMonthIndex = i;
+            }
+            if (budgets.get(i).getMonth().equals(endYearMonth)){
+                endMonthIndex = i;
+                break;
+            }
+        }
+
+        for (int i = startMonthIndex; i <= endMonthIndex; i++){
+            finalBudget += budgets.get(i).getAmount();
+        }
+
+        finalBudget -= budgets.get(startMonthIndex).getAmount() / start.lengthOfMonth() * startMonthNonBudget;
+        finalBudget -= budgets.get(endMonthIndex).getAmount() /end.lengthOfMonth() * endMonthNonBudget;
+
+        return finalBudget;
     }
+
 }
--- src/test/java/com/odde/tdd/BudgetPlanTest.java
+++ src/test/java/com/odde/tdd/BudgetPlanTest.java
@@ -1,20 +1,67 @@
 package com.odde.tdd;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Collections;
+import java.time.YearMonth;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class BudgetPlanTest {
+public class BudgetPlanTest {
+
     @Test
-    void no_budget() {
+    public void no_budget(){
         BudgetRepo repo = mock(BudgetRepo.class);
         when(repo.findAll()).thenReturn(Collections.emptyList());
         BudgetPlan plan = new BudgetPlan(repo);
-        assertEquals(0, plan.query(LocalDate.of(2023, 4, 20), LocalDate.of(2023, 6, 4)));
+        assertEquals(0, plan.query(LocalDate.of(2023,3,20), LocalDate.of(2023, 4,14)));
+    }
+
+    @Test
+    public void full_month_budget(){
+        BudgetRepo repo = mock(BudgetRepo.class);
+        when(repo.findAll()).thenReturn(prepareBudget());
+        BudgetPlan plan = new BudgetPlan(repo);
+        assertEquals(3100, plan.query(LocalDate.of(2023,5,1), LocalDate.of(2023, 5,31)));
+    }
+
+    @Test
+    public void part_start_month_budget(){
+        BudgetRepo repo = mock(BudgetRepo.class);
+        when(repo.findAll()).thenReturn(prepareBudget());
+        BudgetPlan plan = new BudgetPlan(repo);
+        assertEquals(1200, plan.query(LocalDate.of(2023,5,20), LocalDate.of(2023, 5,31)));
+    }
+
+    @Test
+    public void part_end_month_budget(){
+        BudgetRepo repo = mock(BudgetRepo.class);
+
+        when(repo.findAll()).thenReturn(prepareBudget());
+        BudgetPlan plan = new BudgetPlan(repo);
+        assertEquals(2800, plan.query(LocalDate.of(2023,7,1), LocalDate.of(2023, 7,14)));
+    }
+
+    @Test
+    public void full_during_budget(){
+        BudgetRepo repo = mock(BudgetRepo.class);
+
+        when(repo.findAll()).thenReturn(prepareBudget());
+        BudgetPlan plan = new BudgetPlan(repo);
+        assertEquals(4300, plan.query(LocalDate.of(2023,5,20), LocalDate.of(2023, 7,14)));
+    }
+
+    public List<Budget> prepareBudget(){
+        List<Budget> budgets = new ArrayList<Budget>();
+        budgets.add(new Budget(YearMonth.of(2023,4), 600));
+        budgets.add(new Budget(YearMonth.of(2023,5), 3100));
+        budgets.add(new Budget(YearMonth.of(2023,6), 300));
+        budgets.add(new Budget(YearMonth.of(2023,7), 6200));
+        budgets.add(new Budget(YearMonth.of(2023,8), 620));
+        return budgets;
     }
 }
